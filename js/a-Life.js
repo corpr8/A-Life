fef266f634bb6a6ce03f1e3db70c2736cdf24e15
Paul Edwards <corpr8@hotmail.com>
2013-09-10 18:25:26 +0100

    fixing namespace

--- js/a-Life.js
+++ js/a-Life.js
@@ -1,7 +1,7 @@
 //now the alife...
 //Paul Edwards 2013
-
-var circles = [];
+var aLife = [];
+aLife.circles = [];
 var maxAnimationDuration = 1000;
 var minAnimationDuration = 500;
 var maxDiameter = 80;
@@ -41,7 +41,7 @@ $(document).ready(function() {
 
 	function countCircles() {
 		var returnVal = 0;
-		for (circle in circles) {
+		for (circle in aLife.circles) {
 			returnVal += 1
 		};
 		return (returnVal);
@@ -49,8 +49,8 @@ $(document).ready(function() {
 
 	function anythingAtLocation(locationX, locationY) {
 		var returnVal = false;
-		for (circle in circles) {
-			if (circles[circle].attr('cx') == locationX && circles[circle].attr('cy') == locationY) {
+		for (circle in aLife.circles) {
+			if (aLife.circles[circle].attr('cx') == locationX && aLife.circles[circle].attr('cy') == locationY) {
 				returnVal = true;
 			}
 		}
@@ -64,34 +64,34 @@ $(document).ready(function() {
 
 	function checkSpawn(circleUUID) {
 		//console.log('check spawn');
-		//console.log('neighbours: ' + circles[circleUUID].countNeighbours());
-		if (circles[circleUUID].countNeighbours() < 3) {
+		//console.log('neighbours: ' + aLife.circles[circleUUID].countNeighbours());
+		if (aLife.circles[circleUUID].countNeighbours() < 3) {
 			var myChoice = Math.floor(Math.random() * 8);
 			//    console.log("choice: " + myChoice);
 			switch(myChoice) {
 				case 0:
-					createAnimateCircle(null, circles[circleUUID].posTop().x, circles[circleUUID].posTop().y);
+					createAnimateCircle(null, aLife.circles[circleUUID].posTop().x, aLife.circles[circleUUID].posTop().y);
 					break;
 				case 1:
-					createAnimateCircle(null, circles[circleUUID].posTopRight().x, circles[circleUUID].posTopRight().y);
+					createAnimateCircle(null, aLife.circles[circleUUID].posTopRight().x, aLife.circles[circleUUID].posTopRight().y);
 					break;
 				case 2:
-					createAnimateCircle(null, circles[circleUUID].posRight().x, circles[circleUUID].posRight().y);
+					createAnimateCircle(null, aLife.circles[circleUUID].posRight().x, aLife.circles[circleUUID].posRight().y);
 					break;
 				case 3:
-					createAnimateCircle(null, circles[circleUUID].posBottomRight().x, circles[circleUUID].posBottomRight().y);
+					createAnimateCircle(null, aLife.circles[circleUUID].posBottomRight().x, aLife.circles[circleUUID].posBottomRight().y);
 					break;
 				case 4:
-					createAnimateCircle(null, circles[circleUUID].posBottom().x, circles[circleUUID].posBottom().y);
+					createAnimateCircle(null, aLife.circles[circleUUID].posBottom().x, aLife.circles[circleUUID].posBottom().y);
 					break;
 				case 5:
-					createAnimateCircle(null, circles[circleUUID].posBottomLeft().x, circles[circleUUID].posBottomLeft().y);
+					createAnimateCircle(null, aLife.circles[circleUUID].posBottomLeft().x, aLife.circles[circleUUID].posBottomLeft().y);
 					break;
 				case 6:
-					createAnimateCircle(null, circles[circleUUID].posLeft().x, circles[circleUUID].posLeft().y);
+					createAnimateCircle(null, aLife.circles[circleUUID].posLeft().x, aLife.circles[circleUUID].posLeft().y);
 					break;
 				case 7:
-					createAnimateCircle(null, circles[circleUUID].posTopLeft().x, circles[circleUUID].posTopLeft().y);
+					createAnimateCircle(null, aLife.circles[circleUUID].posTopLeft().x, aLife.circles[circleUUID].posTopLeft().y);
 					break;
 
 			}
@@ -99,21 +99,21 @@ $(document).ready(function() {
 			//had too many neighbours...
 			//    console.log('too many neighbours');
 			try {
-				circles[circleUUID].stop();
-				circles[circleUUID].animate({
+				aLife.circles[circleUUID].stop();
+				aLife.circles[circleUUID].animate({
 					"opacity" : 0,
 					"stroke-opacity" : 0,
 					"r" : 0
 				}, (thisAnimationDuration / 2), "linear", function() {
-					//circles[circleUUID].clear();
+					//aLife.circles[circleUUID].clear();
 					//
 
-					var indexToRemove = circles[circleUUID].thisIndex();
+					var indexToRemove = aLife.circles[circleUUID].thisIndex();
 					if (indexToRemove > -1) {
 						//console.log('found index to remove ' + indexToRemove);
-						//circles.splice(indexToRemove, 1);
-						circles[circleUUID].remove();
-						delete circles[circleUUID];
+						//aLife.circles.splice(indexToRemove, 1);
+						aLife.circles[circleUUID].remove();
+						delete aLife.circles[circleUUID];
 						//          paper.splice(indexToRemove,0);
 
 					}
@@ -142,7 +142,7 @@ $(document).ready(function() {
 			}
 			try {
 				//circle existed
-				circles[circleUUID].animate({
+				aLife.circles[circleUUID].animate({
 					fill : getRandomColor(),
 					stroke : getRandomColor(),
 					"stroke-width" : getRandomWidth(),
@@ -151,7 +151,7 @@ $(document).ready(function() {
 					"r" : getRandomDiameter()
 				}, (thisAnimationDuration / 2), "linear", function() {
 					try {
-						circles[circleUUID].animate({
+						aLife.circles[circleUUID].animate({
 							fill : getRandomColor(),
 							stroke : getRandomColor(),
 							"stroke-width" : getRandomWidth(),
@@ -178,94 +178,94 @@ $(document).ready(function() {
 				if (anythingAtLocation(circleX, circleY) || circleX < 0 || circleY < 0 || circleX > $(document).width() || circleY > $(document).height()) {
 					return;
 				}
-				circles.push();
-				circles[circleUUID] = paper.circle(circleX, circleY, 1);
+				aLife.circles.push();
+				aLife.circles[circleUUID] = paper.circle(circleX, circleY, 1);
 			} else {
-				circles.push();
+				aLife.circles.push();
 				//      console.log('no xy');
-				circles[circleUUID] = paper.circle(($(document).width() / 2), ($(document).height() / 2), 1);
+				aLife.circles[circleUUID] = paper.circle(($(document).width() / 2), ($(document).height() / 2), 1);
 			}
 
-			circles[circleUUID].node.id = circleUUID;
+			aLife.circles[circleUUID].node.id = circleUUID;
 
-			circles[circleUUID].posTop = function() {
+			aLife.circles[circleUUID].posTop = function() {
 				var returnVal = [];
-				returnVal.x = circles[circleUUID].attr('cx');
-				returnVal.y = circles[circleUUID].attr('cy') - spacing;
+				returnVal.x = aLife.circles[circleUUID].attr('cx');
+				returnVal.y = aLife.circles[circleUUID].attr('cy') - spacing;
 				return (returnVal);
 			};
 
-			circles[circleUUID].posTopRight = function() {
+			aLife.circles[circleUUID].posTopRight = function() {
 				var returnVal = [];
-				returnVal.x = circles[circleUUID].attr('cx') + spacing;
-				returnVal.y = circles[circleUUID].attr('cy') - spacing;
+				returnVal.x = aLife.circles[circleUUID].attr('cx') + spacing;
+				returnVal.y = aLife.circles[circleUUID].attr('cy') - spacing;
 				return (returnVal);
 			};
 
-			circles[circleUUID].posRight = function() {
+			aLife.circles[circleUUID].posRight = function() {
 				var returnVal = [];
-				returnVal.x = circles[circleUUID].attr('cx') + spacing;
-				returnVal.y = circles[circleUUID].attr('cy');
+				returnVal.x = aLife.circles[circleUUID].attr('cx') + spacing;
+				returnVal.y = aLife.circles[circleUUID].attr('cy');
 				return (returnVal);
 			};
 
-			circles[circleUUID].posBottomRight = function() {
+			aLife.circles[circleUUID].posBottomRight = function() {
 				var returnVal = [];
-				returnVal.x = circles[circleUUID].attr('cx') + spacing;
-				returnVal.y = circles[circleUUID].attr('cy') + spacing;
+				returnVal.x = aLife.circles[circleUUID].attr('cx') + spacing;
+				returnVal.y = aLife.circles[circleUUID].attr('cy') + spacing;
 				return (returnVal);
 			};
 
-			circles[circleUUID].posBottom = function() {
+			aLife.circles[circleUUID].posBottom = function() {
 				var returnVal = [];
-				returnVal.x = circles[circleUUID].attr('cx');
-				returnVal.y = circles[circleUUID].attr('cy') + spacing;
+				returnVal.x = aLife.circles[circleUUID].attr('cx');
+				returnVal.y = aLife.circles[circleUUID].attr('cy') + spacing;
 				return (returnVal);
 			};
 
-			circles[circleUUID].posBottomLeft = function() {
+			aLife.circles[circleUUID].posBottomLeft = function() {
 				var returnVal = [];
-				returnVal.x = circles[circleUUID].attr('cx') - spacing;
-				returnVal.y = circles[circleUUID].attr('cy') + spacing;
+				returnVal.x = aLife.circles[circleUUID].attr('cx') - spacing;
+				returnVal.y = aLife.circles[circleUUID].attr('cy') + spacing;
 				return (returnVal);
 			};
 
-			circles[circleUUID].posLeft = function() {
+			aLife.circles[circleUUID].posLeft = function() {
 				var returnVal = [];
-				returnVal.x = circles[circleUUID].attr('cx') - spacing;
-				returnVal.y = circles[circleUUID].attr('cy');
+				returnVal.x = aLife.circles[circleUUID].attr('cx') - spacing;
+				returnVal.y = aLife.circles[circleUUID].attr('cy');
 				return (returnVal);
 			};
 
-			circles[circleUUID].posTopLeft = function() {
+			aLife.circles[circleUUID].posTopLeft = function() {
 				var returnVal = [];
-				returnVal.x = circles[circleUUID].attr('cx') - spacing;
-				returnVal.y = circles[circleUUID].attr('cy') - spacing;
+				returnVal.x = aLife.circles[circleUUID].attr('cx') - spacing;
+				returnVal.y = aLife.circles[circleUUID].attr('cy') - spacing;
 				return (returnVal);
 			};
 
-			circles[circleUUID].countNeighbours = function() {
+			aLife.circles[circleUUID].countNeighbours = function() {
 				var returnVal = 1;
 
-				//for(circle in circles){console.log(circles[circle].attr('cx'))}
+				//for(circle in aLife.circles){console.log(aLife.circles[circle].attr('cx'))}
 
-				for (circle in circles) {
-					if ((circles[circle].attr('cx') == circles[circleUUID].posTop().x && circles[circle].attr('cy') == circles[circleUUID].posTop().y) || (circles[circle].attr('cx') == circles[circleUUID].posTopRight().x && circles[circle].attr('cy') == circles[circleUUID].posTopRight().y) || (circles[circle].attr('cx') == circles[circleUUID].posRight().x && circles[circle].attr('cy') == circles[circleUUID].posRight().y) || (circles[circle].attr('cx') == circles[circleUUID].posBottomRight().x && circles[circle].attr('cy') == circles[circleUUID].posBottomRight().y) || (circles[circle].attr('cx') == circles[circleUUID].posBottom().x && circles[circle].attr('cy') == circles[circleUUID].posBottom().y) || (circles[circle].attr('cx') == circles[circleUUID].posBottomLeft().x && circles[circle].attr('cy') == circles[circleUUID].posBottomLeft().y) || (circles[circle].attr('cx') == circles[circleUUID].posLeft().x && circles[circle].attr('cy') == circles[circleUUID].posLeft().y) || (circles[circle].attr('cx') == circles[circleUUID].posTopLeft().x && circles[circle].attr('cy') == circles[circleUUID].posTopLeft().y)) {
+				for (circle in aLife.circles) {
+					if ((aLife.circles[circle].attr('cx') == aLife.circles[circleUUID].posTop().x && aLife.circles[circle].attr('cy') == aLife.circles[circleUUID].posTop().y) || (aLife.circles[circle].attr('cx') == aLife.circles[circleUUID].posTopRight().x && aLife.circles[circle].attr('cy') == aLife.circles[circleUUID].posTopRight().y) || (aLife.circles[circle].attr('cx') == aLife.circles[circleUUID].posRight().x && aLife.circles[circle].attr('cy') == aLife.circles[circleUUID].posRight().y) || (aLife.circles[circle].attr('cx') == aLife.circles[circleUUID].posBottomRight().x && aLife.circles[circle].attr('cy') == aLife.circles[circleUUID].posBottomRight().y) || (aLife.circles[circle].attr('cx') == aLife.circles[circleUUID].posBottom().x && aLife.circles[circle].attr('cy') == aLife.circles[circleUUID].posBottom().y) || (aLife.circles[circle].attr('cx') == aLife.circles[circleUUID].posBottomLeft().x && aLife.circles[circle].attr('cy') == aLife.circles[circleUUID].posBottomLeft().y) || (aLife.circles[circle].attr('cx') == aLife.circles[circleUUID].posLeft().x && aLife.circles[circle].attr('cy') == aLife.circles[circleUUID].posLeft().y) || (aLife.circles[circle].attr('cx') == aLife.circles[circleUUID].posTopLeft().x && aLife.circles[circle].attr('cy') == aLife.circles[circleUUID].posTopLeft().y)) {
 						returnVal += 1;
 					}
 				}
-				//returnVal.x = circles[circleUUID].attr('cx') - spacing;
-				//returnVal.y = circles[circleUUID].attr('cy') - spacing;
+				//returnVal.x = aLife.circles[circleUUID].attr('cx') - spacing;
+				//returnVal.y = aLife.circles[circleUUID].attr('cy') - spacing;
 				return (returnVal);
 			};
 
-			circles[circleUUID].thisIndex = function() {
+			aLife.circles[circleUUID].thisIndex = function() {
 				//console.log('searching index');
 				var returnVal = -1;
 				var currentIndex = 0;
-				for (circle in circles) {
-					if (circles[circle].node) {
-						if (circles[circle].node.id == circleUUID) {
+				for (circle in aLife.circles) {
+					if (aLife.circles[circle].node) {
+						if (aLife.circles[circle].node.id == circleUUID) {
 							//console.log('found node');
 							returnVal = currentIndex;
 						}
